1ac5d86727c76d32d25f595b48b2aa661849a528
Sharabesh Ramesh <sharabesh@berkeley.edu>
2017-01-25 16:53:39 -0800

    Updated main.py to include old code

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import Bio
-import sys
-from Bio import SwissProt as sp
-from  Bio import SeqIO
-from urllib.parse import urlparse
-import urllib
-import re
-from Bio.SubsMat.MatrixInfo import blosum62 as bs
-import sys
+from Bio import SeqIO
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+from Bio.Blast import NCBIXML
+from Bio.Blast.NCBIWWW import qblast
+from Bio.Align.Applications import MafftCommandline
+from Bio.Align import MultipleSeqAlignment
 import os
 
-#BLAST XML Reader
-from Bio.Blast import NCBIXML
-
-#BLAST Command Line Tools (Python Wrapper)
-from Bio.Blast.Applications import NcbiblastpCommandline
 
 #Ensures environment variables are set
 assert os.environ.get("BLASTDB")!=None, "Directory of Blast database must be set"
@@ -43,10 +39,27 @@ class Sequence:
             output += item.aa
         return output
 
-"""Uses BioPython rather than System os"""
-def gather_homologs(fasta): #File location passed as input ("fasta.txt")
-    blasted = NcbiblastpCommandline(query=fasta,db="nr.61",evalue=1,outfmt=5,out="alignment.xml")
-    blasted()
+E_VAL_THRESH = .05
+ALIGN_PERCENT_THRESH = 80
+DATABASE = "nr.61"
+CONSENSUS_THRESH = .5
+INPUT_SEQUENCE = "fasta.txt"
+save_file_name = "alignment.xml"
+recs_file = "records.fasta"
+
+
+def length(fasta):
+    input = SeqIO.read("fasta.txt",format="fasta")
+    return len(input)
+
+
+input_len = length("fasta.txt")
+
+#
+# """Uses BioPython rather than System os"""
+# def gather_homologs(fasta): #File location passed as input ("fasta.txt")
+#     blasted = NcbiblastpCommandline(query=fasta,db="nr.61",evalue=1,outfmt=5,out="alignment.xml")
+#     blasted()
 
 
 """Using System os
@@ -54,20 +67,35 @@ def gather_homologs(fasta): #File location passed as input ("fasta.txt")
 def sys_gather_homologs(fasta):
     generic = "blastp -db {0} -query {1} -evalue {2}" \
               " -outfmt {3} -out {4} -qcov_hsp_perc {5}"
-    db="nr.61"
+    db=DATABASE
     query = fasta
-    evalue = 0.01 #Change if wanted
+    evalue = E_VAL_THRESH #Change if wanted
     outfmt = 5 # Indicates BLAST XML to write to file
     out = "alignment.xml" #Data written to alignment.xml
-    query_coverage = 80 #Minimum required overlap
+    query_coverage = ALIGN_PERCENT_THRESH #Minimum required overlap
     formatted = generic.format(db,query,evalue,outfmt,out,query_coverage)
     os.system(formatted)
 
 
-def parse_xml(alignment_file): #This is alignment.xml
-    record_handle = open(alignment_file)
-    records = NCBIXML.parse(record_handle)
-    return records
 
-def generate_msa(records_lst): #Takes output from parse_xml
-    pass
+def parse_xml(): #This is alignment.xml
+    with open(save_file_name) as result_handle:
+        blast_record = NCBIXML.read(result_handle)
+    recs = []
+    i = 0
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            filtered_seq = Seq(str(hsp.sbjct), IUPAC.protein)
+            filtered_id = alignment.title.split("|")[3]
+            filtered_name = alignment.title
+            filtered_seqrec = SeqRecord(filtered_seq, id=filtered_id, name=filtered_name)
+            recs.append(filtered_seqrec)
+        print(alignment)
+    _ = SeqIO.write(recs, recs_file, "fasta")
+    print("Done")
+
+def generate_msa():
+    mafft_cline = MafftCommandline(input=recs_file)
+    stdout, stderr = mafft_cline()
+    with open(recs_file, "w") as handle:
+        handle.write(stdout)
